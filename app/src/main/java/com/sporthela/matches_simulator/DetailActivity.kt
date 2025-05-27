0cc47086c26e9106d1052e36a96e543f37598d43
jesse <nitchsx@gmail.com>
2025-05-27 16:16:16 -0300

    feat: include new layouts and new themes,colors and dimens

---
+++ app/src/main/java/com/sporthela/matches_simulator/DetailActivity.kt
@@ -0,0 +1,23 @@
+package com.sporthela.matches_simulator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sporthela.matches_simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
